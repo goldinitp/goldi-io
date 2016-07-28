4d0c7f23b5d1a93dcae14f60cb64a1b6a7ea6dcf
goldinitp <goldinitp@gmail.com>
2016-07-28 09:46:35 +0530

    403

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	 <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+ ?>
